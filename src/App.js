46c5aea80581bbe347f8b63ebd8492379a1d1466
PsychoXgod <Lobanov2000k@yandex.ru>
2021-06-04 22:04:40 +0500

    add sort by type

--- src/App.js
+++ src/App.js
@@ -5,6 +5,78 @@ import styled from 'styled-components';
 import { useDispatch, useSelector } from 'react-redux';
 import { setStudents } from './redux/actions/students';
 
+function App() {
+  const dispatch = useDispatch();
+  const { students, input, sortType } = useSelector(({ students }) => {
+    return {
+      students: students.students,
+      input: students.input,
+      sortType: students.sortType,
+    };
+  });
+
+  React.useEffect(() => {
+    fetch('https://front-assignment-api.2tapp.cc/api/persons')
+      .then((resp) => resp.json())
+      .then((json) => {
+        dispatch(setStudents(json.students));
+      });
+  }, []);
+
+  function getAge(birthday) {
+    //   день рождения в формате Год-месяц-день
+    const [year, month, day] = birthday.split('-');
+    const today = new Date(); // сегодняшняя дата
+    const dateBirthday = new Date(year, month, day); // дата рождения в формате Date
+    let age = today.getFullYear() - dateBirthday.getFullYear();
+    const m = today.getMonth() - (dateBirthday.getMonth() - 1); // вычитаем, т.к. в JSе отсчет месяцев начинается с 0
+
+    return m < 0 || (m === 0 && today.getDate() < dateBirthday.getDate()) ? --age : age;
+  }
+
+  function sortBy(a, b, type) {
+    switch (type) {
+      case 'Имя А-Я':
+        return a.name.localeCompare(b.name);
+      case 'Имя Я-А':
+        return b.name.localeCompare(a.name);
+      case 'Сначала моложе':
+        return getAge(a.birthday) - getAge(b.birthday);
+      case 'Сначала старше':
+        return getAge(b.birthday) - getAge(a.birthday);
+      case 'Низкий рейтинг':
+        return a.rating - b.rating;
+      case 'Высокий рейтинг':
+        return b.rating - a.rating;
+      default:
+        return;
+    }
+  }
+
+  return (
+    <div className="App">
+      <Header />
+      <MainContent>
+        <AppTitle>Студенты</AppTitle>
+        <Search />
+        <StudentWrapper>
+          <StudentName>ФИО</StudentName>
+          <StudentSpecialty>Специальность</StudentSpecialty>
+          <StudentGroup>Группа</StudentGroup>
+          <StudentAge>Возраст</StudentAge>
+          <StudentRating>Рейтинг</StudentRating>
+        </StudentWrapper>
+        <MainStudentsWrapper>
+          {students &&
+            students
+              .filter((student) => student.name.toLowerCase().includes(input.toLowerCase()))
+              .sort((a, b) => sortBy(a, b, sortType))
+              .map((student) => <Students key={student.id} {...student} />)}
+        </MainStudentsWrapper>
+      </MainContent>
+    </div>
+  );
+}
 const AppTitle = styled.h1`
   color: ${(props) => props.theme.colors.primary};
   text-align: start;
@@ -82,44 +154,4 @@ const StudentRating = styled.h2`
   width: 6.8%;
   margin-right: 3.4%;
 `;
-
-function App() {
-  const dispatch = useDispatch();
-  const { students, input } = useSelector(({ students }) => {
-    return {
-      students: students.students,
-      input: students.input,
-    };
-  });
-  React.useEffect(() => {
-    fetch('https://front-assignment-api.2tapp.cc/api/persons')
-      .then((resp) => resp.json())
-      .then((json) => {
-        dispatch(setStudents(json.students));
-      });
-  }, []);
-  return (
-    <div className="App">
-      <Header />
-      <MainContent>
-        <AppTitle>Студенты</AppTitle>
-        <Search />
-        <StudentWrapper>
-          <StudentName>ФИО</StudentName>
-          <StudentSpecialty>Специальность</StudentSpecialty>
-          <StudentGroup>Группа</StudentGroup>
-          <StudentAge>Возраст</StudentAge>
-          <StudentRating>Рейтинг</StudentRating>
-        </StudentWrapper>
-        <MainStudentsWrapper>
-          {students &&
-            students
-              .filter((student) => student.name.toLowerCase().includes(input.toLowerCase()))
-              .map((student) => <Students key={student.id} {...student} />)}
-        </MainStudentsWrapper>
-      </MainContent>
-    </div>
-  );
-}
-
 export default App;
